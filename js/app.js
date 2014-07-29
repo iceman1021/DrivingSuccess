35ce924b8308c5884733021037bd9d907ae02acb
Llewellyn <llewellyntd@gmail.com>
2014-07-29 15:05:20 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -23,6 +23,49 @@ function onError(error) {
           'message: ' + error.message + '\n');
 }
 
+//--External URL--//
+function loadURL(url){ 
+    if (devicePlatform == 'Android') {
+        navigator.app.loadUrl(url, { openExternal:true }); 
+    }
+    else
+    {
+        window.open(url, '_system', 'location=no');
+        return false;
+    }
+}
+
+// Load user points and gauge //
+function loadPoints(uid) {
+    console.log('Igot here and the uid is '+uid);
+
+    $.ajax({
+        type       : "POST",
+        url        : formurl,
+        crossDomain: true,
+        beforeSend : function() {$.mobile.loading('show')},
+        complete   : function() {$.mobile.loading('hide')},
+        data       : "tipe=getPoints&uid="+uid,
+        dataType   : 'json',
+        success    : function(response) {
+            var gaugeImage = response["gaugeImage"];
+            var pointsPre = response["pointsPre"];
+            var pointsNow = response["pointsNow"];
+            
+            $('#gaugeImage').empty();
+            $('#gaugeImage').append(gaugeImage); 
+            $('#pointsPre').empty();
+            $('#pointsPre').append(pointsPre);
+            $('#pointsNow').empty();
+            $('#pointsNow').append(pointsNow);
+        },
+        error      : function() {
+            console.error("error");
+            alert('Unable to connect to server, please try again...');                  
+        }
+    });
+}
+
 $(document).on("pageshow", "#dashboard", function(){ 
     
     loginName = localStorage.getItem('log_name');
@@ -32,6 +75,71 @@ $(document).on("pageshow", "#dashboard", function(){
     $('#userName').append(loginName);
     $('#logSurname').empty();
     $('#logSurname').append(loginSurname);
+});
+
+$(document).on("pageshow", "#rewards", function(){ 
+    loadPoints(localStorage.getItem('log_uid'));
+    console.log('Igot here and the uid is '+localStorage.getItem('log_uid'));
+});
+
+$(document).on("pageshow", "#ranking", function(){ 
+    
+});
+
+$(document).on("pageshow", "#whatsnew", function(){ 
+    
+});
+
+$(document).on("pageshow", "#logout", function(){
+    localStorage.log_name = '';
+    localStorage.log_surname = '';
+    localStorage.log_uid = '';
+    localStorage.log_remeber = '';
+    localStorage.log_email = '';
+    
+    $('#loginForm').submit(function(e)
+    {
+        console.log($(this).serialize());
+        loading = true;
+        $.ajax({
+            type       : "POST",
+            url        : formurl,
+            crossDomain: true,
+            beforeSend : function() {$.mobile.loading('show')},
+            complete   : function() {$.mobile.loading('hide')},
+            data       : "tipe=login&"+$(this).serialize(),
+            dataType   : 'json',
+            success    : function(response) {
+                
+                var resultHtml = response["html"];
+                var resultError = response["error"];
+                loginName = response['name'];
+                loginSurname = response['surname'];
+                loginUID = response['user_id'];
+                loginRemember = response['remember'];
+                loginEmail = response['email'];
+                
+                if (resultError == '1') {
+                    $('#loginError').empty();
+                    $('#loginError').append(resultHtml);  
+                    $('#loginError').popup("open", {transition: "slidedown"});
+                } else {
+                    localStorage.log_name = loginName;
+                    localStorage.log_surname = loginSurname;
+                    localStorage.log_uid = loginUID;
+                    localStorage.log_remeber = loginRemember;
+                    localStorage.log_email = loginEmail;
+
+                    $.mobile.changePage("dashboard.html#dashboard");
+                }
+            },
+            error      : function() {
+                console.error("error");
+                alert('Unable to connect to server, please try again...');                  
+            }
+        });
+        e.preventDefault();
+    });
 });
 
 $(document).on("pageshow", "#login", function(){ 
@@ -83,8 +191,6 @@ $(document).on("pageshow", "#login", function(){
 
 $(document).on("pageshow", "#forgot", function(){ 
     
-    $("#content-body-main").height(contentH);
-    
     $('#forgotForm').submit(function(e)
     {
         console.log($(this).serialize());
@@ -121,8 +227,6 @@ $(document).on("pageshow", "#forgot", function(){
 });
 
 $(document).on("pageshow", "#signup", function(){ 
-    
-    $("#content-body-main").height(contentH);
     
     $('#signupForm').submit(function(e)
     {
